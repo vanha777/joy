48b834744f5b7db4dc00e0722845fa417086423d
vanha777 <vanha777@icloud.com>
2023-11-18 03:07:58 +1100

    add colour theme & img generator

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 extern crate dotenv;
+pub mod models;
 use dotenv::dotenv;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, LOCATION};
+use reqwest::{self, header};
+use serde_json;
 use std::env;
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -13,10 +17,16 @@ lazy_static::lazy_static! {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![my_custom_command, get_server_state,change_server_state])
+        .invoke_handler(tauri::generate_handler![
+            my_custom_command,
+            get_server_state,
+            change_server_state,
+            image_request
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
 #[tauri::command]
 fn my_custom_command(invoke_message: &str) -> String {
     format!(
@@ -26,9 +36,50 @@ fn my_custom_command(invoke_message: &str) -> String {
 }
 
 #[tauri::command]
+async fn image_request(payload: models::ImagePayload) -> Result<serde_json::Value, String> {
+    dotenv().ok();
+    let endpoint = std::env::var("MADCATZ_SERVER")
+        .map_err(|e| format!("Failed to read MADCATZ_SERVER environment variable: {}", e))?;
+
+    let body = serde_json::json!({
+        "input": payload.invoke_message.to_string()
+    });
+    let token = &payload.access_token.clone();
+    let user = &payload.user_id.clone();
+    // Create a custom HTTP header
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(token).map_err(|e| e.to_string())?,
+    );
+    headers.insert(
+        LOCATION,
+        HeaderValue::from_str(user).map_err(|e| e.to_string())?,
+    );
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/image", endpoint))
+        .headers(headers)
+        .json(&body) // Serialize the JSON body to a string
+        .send()
+        .await
+        .map_err(|e| format!("Failed to send HTTP request: {}", e))?;
+
+    let result_text = response
+        .json()
+        .await
+        .map_err(|e| format!("Failed to read response body: {}", e))?;
+
+    Ok(result_text)
+}
+
+#[tauri::command]
 fn get_server_state() -> String {
     let state = STATE.lock().unwrap();
-    format!("This is the message from Rust invoked from front-end: {}!", *state)
+    format!(
+        "This is the message from Rust invoked from front-end: {}!",
+        *state
+    )
 }
 
 #[tauri::command]
--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,7 +1,15 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+use serde::{Serialize, Deserialize};
+
 struct ServerState {
     clients_connected: u32,
     // Add other state fields as needed
+}
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct ImagePayload {
+    pub invoke_message: String,
+    pub access_token: String,
+    pub user_id: String, // Add other state fields as needed
 }
