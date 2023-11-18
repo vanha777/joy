ac4c5ca4c7ed85aa0bfdfeb10ab8173f69e096b6
vanha777 <vanha777@icloud.com>
2023-11-18 20:01:47 +1100

    durian

---
+++ src/Dashboard/PPcManager/PPcManager.tsx
@@ -0,0 +1,142 @@
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab, { tabClasses } from '@mui/joy/Tab';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+
+import TabPanel from '@mui/joy/TabPanel';
+import TeamPages from '../TeamPages';
+import BrandScape from '../BrandScape';
+import DriveDynamics from '../DriveDynamics';
+
+export default function ContentHub() {
+  return (
+    <Box
+      sx={{
+        flex: 1,
+        width: '100%',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'sticky',
+          top: {
+            sm: -100,
+            md: -110,
+          },
+          bgcolor: 'background.body',
+          zIndex: 9995,
+        }}
+      >
+        <Box
+          sx={{
+            px: {
+              xs: 2,
+              md: 6,
+            },
+          }}
+        >
+          <Breadcrumbs
+            size="sm"
+            aria-label="breadcrumbs"
+            separator={<ChevronRightRoundedIcon fontSize="small" />}
+            sx={{ pl: 0 }}
+          >
+            <Link
+              underline="none"
+              color="neutral"
+              href="#some-link"
+              aria-label="Home"
+            >
+              <HomeRoundedIcon />
+            </Link>
+            <Link
+              underline="hover"
+              color="neutral"
+              href="#some-link"
+              fontSize={12}
+              fontWeight={500}
+            >
+              Users
+            </Link>
+            <Typography color="primary" fontWeight={500} fontSize={12}>
+              My profile
+            </Typography>
+          </Breadcrumbs>
+          <Typography
+            level="h2"
+            sx={{
+              mt: 1,
+              mb: 2,
+            }}
+          >
+            Content Commander
+          </Typography>
+        </Box>
+        <Tabs
+          defaultValue={0}
+          sx={{
+            bgcolor: 'transparent',
+          }}
+        >
+
+          <TabList
+            tabFlex={1}
+            size="sm"
+            sx={{
+              pl: {
+                xs: 0,
+                md: 4,
+              },
+              justifyContent: 'left',
+              [`&& .${tabClasses.root}`]: {
+                flex: 'initial',
+                bgcolor: 'transparent',
+                [`&.${tabClasses.selected}`]: {
+                  fontWeight: '600',
+                  '&::after': {
+                    height: '2px',
+                    bgcolor: 'primary.500',
+                  },
+                },
+              },
+            }}
+          >
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={0}>
+              Campaign Setup
+            </Tab>
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={1}>
+              Targeting & Audience
+            </Tab>
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={2}>
+              Client Accounts
+            </Tab>
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={3}>
+              Ad Launch
+            </Tab>
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={4}>
+              Ad Rotation
+            </Tab>
+            <Tab sx={{ borderRadius: '6px 6px 0 0' }} indicatorInset value={5}>
+              ROI Analysis
+            </Tab>
+          </TabList>
+          
+          <TabPanel value={0}>
+          <BrandScape/>
+          </TabPanel>
+
+          <TabPanel value={1}>
+              <DriveDynamics/>
+              </TabPanel>
+    
+                </Tabs>
+          </Box>
+    </Box>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { internalIpV4 } from 'internal-ip'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  
-});
+async function createConfig() {
+  const host = await internalIpV4();
+
+  return defineConfig({
+    plugins: [react()],
+    clearScreen: false,
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        protocol: 'ws',
+        host,
+        port: 5183,
+      },
+    },
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+      target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+      minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+      sourcemap: !!process.env.TAURI_DEBUG,
+    },
+  });
+}
+
+// Export the promise
+export default createConfig();
